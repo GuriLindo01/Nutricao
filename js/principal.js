48ab16908d77a9d2cf4e88b7c6c66d5dce3b8a6a
Gustavo <gustavo.blazius@escola.pr.gov.br>
2025-10-07 09:12:56 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -10,6 +10,7 @@ var peso = tdPeso.textContent;
 console.log(paciente);
 console.log(peso);
 
-var tdAltura = document.querySelector(".info-altura")
+var tdAltura = document.querySelector("#primeiro-paciente");
+var altura = document.querySelector("info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
