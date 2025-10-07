69f0dc2a455c89b60dac39af304f831f2c9acd28
Gustavo <gustavo.blazius@escola.pr.gov.br>
2025-10-07 09:59:35 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -16,7 +16,7 @@ var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
 
-var imc = peso / (altura * altura);
 var tdImc = document.querySelector(".info-imc");
-tdImc.textContent(imc);
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
 console.log(imc);
