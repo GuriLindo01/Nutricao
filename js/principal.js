b089652bf2484ff52ccb00979df3b04723bb8874
Gustavo <gustavo.blazius@escola.pr.gov.br>
2025-10-07 08:34:46 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,14 @@
-        // console.log(document.querySelector("h1"));
-        var titulo = document.querySelector("h1");
-        console.log(titulo);
-        console.log(titulo.textContent);
-        titulo.textContent = ("White Nutritions");
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent=("White Nutritions");
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+console.log(paciente);
+console.log(peso);
+
+var tdAltura = document.querySelector("#primeiro-paciente");
+var altura = document.querySelector(".info-altura");
