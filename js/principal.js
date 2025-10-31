87c2b625d7d410c972ac3bfea979feb29dc30c70
Gustavo <gustavo.blazius@escola.pr.gov.br>
2025-10-31 09:45:47 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 //console.log(document.querySelector("h1"));
 var titulo = document.querySelector("h1");
 console.log(titulo);
@@ -58,3 +60,17 @@ var nome = form.nome.value;
 var peso = form.peso.value;
 var altura = form.altura.value;
 var gordura = form.gordura.value;
+
+var pacienteTr = document.createElement("tr");
+
+var nomeTd = document.createElement("td");
+var pesoTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
+
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura;
+gorduraTd.textContent = gordura;
+// imcTd.textContent = gordura;
